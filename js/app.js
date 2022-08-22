05e7ee74a0e6e48705080ec0094d99228a9ac590
mnalambd09 <mnalambd09@gmail.com>
2022-08-22 18:58:38 +0600

    player name added

--- js/app.js
+++ js/app.js
@@ -17,15 +17,37 @@ function setTextElementValueById(preValue, NewValue){
    
 }
 
-document.getElementById('add-Messi').addEventListener('click', function(){
+function addPlayerName(playerName){
     const addItem = document.getElementById('item-list');
     const li = document.createElement('li');
-    li.innerText = 'Lionel Messi';
-    li.classList.add('item');
+    li.innerText = playerName;
     addItem.appendChild(li);
+}
+
+document.getElementById('add-Messi').addEventListener('click', function(){
+    addPlayerName('Lionel Messi');
+   
   })
-
-
+document.getElementById('add-Neymar').addEventListener('click', function(){
+    addPlayerName('Neymar Jr');
+   
+  })
+document.getElementById('add-Kylian').addEventListener('click', function(){
+    addPlayerName('Kylian Mbapp');
+   
+  })
+document.getElementById('add-Vítor').addEventListener('click', function(){
+    addPlayerName('Vitor Machado');
+   
+  })
+document.getElementById('add-Sergio').addEventListener('click', function(){
+    addPlayerName('Sergio Ramos');
+   
+  })
+document.getElementById('add-Renato').addEventListener('click', function(){
+    addPlayerName('Renato Sanches');
+   
+  })
 
 
 document.getElementById('btn-calculate').addEventListener('click', function(){
@@ -40,8 +62,12 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
 document.getElementById('btn-calculation-total').addEventListener('click', function(){
     const managerBudget = getInputValueById('manager-budget');
     const coachBudget = getInputValueById('coach-Budget');
+    const perPlayerBudget = getInputValueById('per-player');
+
+    const totalPlayer = 5;
+    const playerTotalExpenses = perPlayerBudget * totalPlayer;
     
-    const totalCost = managerBudget + coachBudget;
+    const totalCost = managerBudget + coachBudget + playerTotalExpenses;
     setTextElementValueById('total-cost', totalCost);
 
     
